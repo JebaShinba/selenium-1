901870cb565f748d59796de53b439d7d3689275b
JebaShinba <jebashinba2001@gmail.com>
2024-10-23 16:39:56 +0530

    Test Result3

--- test_google.py
+++ test_google.py
@@ -7,10 +7,12 @@ def test_google_search():
     options.add_argument("--disable-dev-shm-usage")
     
     # Connect to the remote Selenium instance
-    driver = webdriver.Remote(
-        command_executor="http://localhost:4444/wd/hub",
-        options=options
-    )
+    # driver = webdriver.Remote(
+    #     command_executor="http://localhost:4444/wd/hub",
+    #     options=options
+    driver = webdriver.Chrome() 
+    command_executor="http://selenium:4444/wd/hub"
+
     
     driver.get("https://www.google.com")
     assert "Google" in driver.title
